53edd78bcc389f52974d1503a9abd2920d090abf
hooshi@hashimoto <hoshyari@cs.ubc.ca>
2018-03-18 12:18:31 -0700

    Fix formatting

--- as_rigid_as_possible_deformation/mesh/arap.h
+++ as_rigid_as_possible_deformation/mesh/arap.h
@@ -25,11 +25,11 @@
  */
 struct PetscVecWrapper
 {
-	// Petsc Vector.
-	Vec vec;
-	// Pointer to vector data.
-	double *v;
-	PetscVecWrapper():vec(NULL), v(NULL){}
+  // Petsc Vector.
+  Vec vec;
+  // Pointer to vector data.
+  double *v;
+PetscVecWrapper():vec(NULL), v(NULL){}
 };
 
 // Used functionality from the armadillo library.
@@ -43,107 +43,109 @@ typedef arma::mat::fixed<3,3> matrix3d;
  */
 class Deformation
 {
-private:
+ private:
 
-	// Mesh to deform
-	Mesh *_mesh;
-	// Rotation matrices for each vertex
-	arma::field<matrix3d> _rot;
-	// Original location of vertices
-	arma::field<vec3d>    _xyz0;
-	// Current guess for the location of the vertices.
-	arma::field<vec3d>    _xyz;
-	// Right hand side of the linear part of the ARAP system.
-	arma::field<vec3d>    _rhs;
-	// A vector to have random access to each vertex, cause mesh
-	// class only stores them in a list.
-	vector<Vertex*>       _vertIndex;
-	// Weight of each edge.
-	vector<double>        _edgew;
-	// Matrix A, in the ARAP method.
-	Mat                   _A;
-	// Matrix A^T * A in the ARAP method.
-	Mat 				  _ATA;
-	// The vector to get the solution of Ax=b from petsc.
-	PetscVecWrapper       _X;
-	// Right hand side: b.
-	PetscVecWrapper       _B;
-	// A^T * b.
-	PetscVecWrapper       _ATB;
-	// Petsc Linear solver.
-	KSP                   _ksp;
+  // Mesh to deform
+  Mesh *_mesh;
+  // Rotation matrices for each vertex
+  arma::field<matrix3d> _rot;
+  // Original location of vertices
+  arma::field<vec3d>    _xyz0;
+  // Current guess for the location of the vertices.
+  arma::field<vec3d>    _xyz;
+  // Right hand side of the linear part of the ARAP system.
+  arma::field<vec3d>    _rhs;
+  // A vector to have random access to each vertex, cause mesh
+  // class only stores them in a list.
+  vector<Vertex*>       _vertIndex;
+  // Weight of each edge.
+  vector<double>        _edgew;
+  // Matrix A, in the ARAP method.
+  Mat                   _A;
+  // Matrix A^T * A in the ARAP method.
+  Mat 				  _ATA;
+  // The vector to get the solution of Ax=b from petsc.
+  PetscVecWrapper       _X;
+  // Right hand side: b.
+  PetscVecWrapper       _B;
+  // A^T * b.
+  PetscVecWrapper       _ATB;
+  // Petsc Linear solver.
+  KSP                   _ksp;
 
-	// Vertex number and location of the anchor vertices to be
-	// imposed as constraints.
-	map<int, vec3d >   _anchors;
+  // Vertex number and location of the anchor vertices to be
+  // imposed as constraints.
+  map<int, vec3d >   _anchors;
 
-	// Becomes true if the anchor vertices have changed, so that
-	// the matrix has to be assembled and refactored again.
-	bool _should_refactor;
+  // Becomes true if the anchor vertices have changed, so that
+  // the matrix has to be assembled and refactored again.
+  bool _should_refactor;
 
-	// The weight for imposing anchor vertices.
-	double _beta;
-	// Tolerance for checking convergence of the nonlinear solver.
-	double _tol;
-	// Should we use uniform weights for edges?
-	bool _should_use_uniform_wgt;
-	// Maximum number of allowed nonlinear iterations.
-	int _n_max_iter;
-	// Level of verbosity.
-	int _verbosity;
-	// How many deformations we have made so far.
-	int _n_total_deformations;
+  // The weight for imposing anchor vertices.
+  double _beta;
+  // Tolerance for checking convergence of the nonlinear solver.
+  double _tol;
+  // Should we use uniform weights for edges?
+  bool _should_use_uniform_wgt;
+  // Maximum number of allowed nonlinear iterations.
+  int _n_max_iter;
+  // Level of verbosity.
+  int _verbosity;
+  // How many deformations we have made so far.
+  int _n_total_deformations;
 
-	// Assemble the left hand side
-	PetscErrorCode assembleMat();
-	// Assemble the right hand side
-	PetscErrorCode assembleRHS();
-	// Find the rotation matrices again
-	PetscErrorCode updateRots();
+  // Assemble the left hand side
+  PetscErrorCode assembleMat();
+  // Assemble the right hand side
+  PetscErrorCode assembleRHS();
+  // Find the rotation matrices again
+  PetscErrorCode updateRots();
 
-public:
-	// Petsc error code for error checking.
-	PetscErrorCode ierr;
+ public:
+  // Petsc error code for error checking.
+  PetscErrorCode ierr;
 
-	// Constructor and destructor.
-	Deformation(Mesh *mesh_in);
-	PetscErrorCode destroy();
-	~Deformation();
+  // Constructor and destructor.
+  Deformation(Mesh *mesh_in);
+  PetscErrorCode destroy();
+  ~Deformation();
 
-	// THis is just for testing.
-	vector<Vertex*>*  getVertIndex(){return &_vertIndex;}
+  // THis is just for testing.
+  vector<Vertex*>*  getVertIndex(){return &_vertIndex;}
 
-	// Reset the position of vertices in the mesh to their original.
-	PetscErrorCode resetMeshPositions();
+  // Reset the position of vertices in the mesh to their original.
+  PetscErrorCode resetMeshPositions();
 
-	// Deform the mesh
-	// Input: anchorverts: index of anchors.
-	//        newpos:      location of each anchor in order.
-	PetscErrorCode deform(vector<int> anchorverts, vector<float> newpos);
+  // Deform the mesh
+  // Input: anchorverts: index of anchors.
+  //        newpos:      location of each anchor in order.
+  PetscErrorCode deform(vector<int> anchorverts, vector<float> newpos);
 };
 
 /*
  * Functions to move data from Petsc to armadillo and vice versa.
  */
 
-inline void mergeDim(const int dim, PetscVecWrapper in, arma::field<vec3d>& ans)
+inline void
+mergeDim(const int dim, PetscVecWrapper in, arma::field<vec3d>& ans)
 {
-	assert( (dim == 0) || (dim == 1) || (dim == 2) );
+  assert( (dim == 0) || (dim == 1) || (dim == 2) );
 
-	for (int iv = 0 ; iv < (int)ans.size() ; iv++)
-	{
-		ans(iv)(dim) = in.v[iv];
-	}
+  for (int iv = 0 ; iv < (int)ans.size() ; iv++)
+    {
+      ans(iv)(dim) = in.v[iv];
+    }
 }
 
-inline void extractDim(const int dim, arma::field<vec3d>& in, PetscVecWrapper ans)
+inline void
+extractDim(const int dim, arma::field<vec3d>& in, PetscVecWrapper ans)
 {
-	assert( (dim == 0) || (dim == 1) || (dim == 2) );
+  assert( (dim == 0) || (dim == 1) || (dim == 2) );
 
-	for (int iv = 0 ; iv < (int)in.size() ; iv++)
-	{
-		ans.v[iv] = in(iv)(dim);
-	}
+  for (int iv = 0 ; iv < (int)in.size() ; iv++)
+    {
+      ans.v[iv] = in(iv)(dim);
+    }
 }
 
 
